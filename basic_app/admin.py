8beebdb7a1605d8b89201d45cfeab25a16c37768
ulugbekerkinovich2 <ulugbek9015y@gmail.com>
2025-01-10 14:35:05 +0500

    changes on admin

--- basic_app/admin.py
+++ basic_app/admin.py
@@ -1,12 +1,30 @@
 from django.contrib import admin
 from django.utils.html import format_html
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
 from .models import Heartbeat, VerifyPush, ICCardInfoPush, StrangerCapture
+
+# 5 daqiqa
+CACHE_TIMEOUT = 60 * 2
 
 @admin.register(Heartbeat)
 class HeartbeatAdmin(admin.ModelAdmin):
     list_display = ('device_id', 'ip_address', 'mac_address', 'time')
     search_fields = ('device_id', 'ip_address', 'mac_address')
     list_filter = ('time',)
+
+    @method_decorator(cache_page(CACHE_TIMEOUT))
+    def changelist_view(self, request, extra_context=None):
+        return super().changelist_view(request, extra_context=extra_context)
+
+    @method_decorator(cache_page(CACHE_TIMEOUT))
+    def change_view(self, request, object_id, form_url='', extra_context=None):
+        return super().change_view(request, object_id, form_url, extra_context=extra_context)
+
+    @method_decorator(cache_page(CACHE_TIMEOUT))
+    def add_view(self, request, form_url='', extra_context=None):
+        return super().add_view(request, form_url, extra_context=extra_context)
+
 
 @admin.register(VerifyPush)
 class VerifyPushAdmin(admin.ModelAdmin):
@@ -17,19 +35,35 @@ class VerifyPushAdmin(admin.ModelAdmin):
     search_fields = ('person_id', 'name', 'id_card', 'rfid_card', 'mj_card_no')
     list_filter = ('ip_address', 'create_time', 'verify_type', 'direction')
 
+    @method_decorator(cache_page(CACHE_TIMEOUT))
+    def changelist_view(self, request, extra_context=None):
+        return super().changelist_view(request, extra_context=extra_context)
+
+    @method_decorator(cache_page(CACHE_TIMEOUT))
+    def change_view(self, request, object_id, form_url='', extra_context=None):
+        return super().change_view(request, object_id, form_url, extra_context=extra_context)
+
+    @method_decorator(cache_page(CACHE_TIMEOUT))
+    def add_view(self, request, form_url='', extra_context=None):
+        return super().add_view(request, form_url, extra_context=extra_context)
+
+
 @admin.register(ICCardInfoPush)
 class ICCardInfoPushAdmin(admin.ModelAdmin):
     list_display = ('device_id', 'ic_card_num', 'created_at', 'ip_address')
     search_fields = ('device_id', 'ic_card_num')
 
-# @admin.register(StrangerCapture)
-# class StrangerCaptureAdmin(admin.ModelAdmin):
-#     list_display = (
-#         'device_id', 'create_time', 'direction', 
-#         'picture_type', 'operator'
-#     )
-#     search_fields = ('device_id', 'operator')
-#     list_filter = ('create_time', 'direction', 'picture_type')
+    @method_decorator(cache_page(CACHE_TIMEOUT))
+    def changelist_view(self, request, extra_context=None):
+        return super().changelist_view(request, extra_context=extra_context)
+    
+    @method_decorator(cache_page(CACHE_TIMEOUT))
+    def change_view(self, request, object_id, form_url='', extra_context=None):
+        return super().change_view(request, object_id, form_url, extra_context=extra_context)
+
+    @method_decorator(cache_page(CACHE_TIMEOUT))
+    def add_view(self, request, form_url='', extra_context=None):
+        return super().add_view(request, form_url, extra_context=extra_context)
 
 
 @admin.register(StrangerCapture)
@@ -48,5 +82,16 @@ class StrangerCaptureAdmin(admin.ModelAdmin):
                 obj.image_file.url
             )
         return "No Image"
-
     thumbnail.short_description = "Image Preview"
+
+    @method_decorator(cache_page(CACHE_TIMEOUT))
+    def changelist_view(self, request, extra_context=None):
+        return super().changelist_view(request, extra_context=extra_context)
+
+    @method_decorator(cache_page(CACHE_TIMEOUT))
+    def change_view(self, request, object_id, form_url='', extra_context=None):
+        return super().change_view(request, object_id, form_url, extra_context=extra_context)
+
+    @method_decorator(cache_page(CACHE_TIMEOUT))
+    def add_view(self, request, form_url='', extra_context=None):
+        return super().add_view(request, form_url, extra_context=extra_context)
