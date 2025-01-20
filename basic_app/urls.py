b793fdbd9e69c6007250162b4db87fa755a57010
ulugbekerkinovich2 <ulugbek9015y@gmail.com>
2025-01-20 17:49:13 +0500

    add two urls

--- basic_app/urls.py
+++ basic_app/urls.py
@@ -3,7 +3,10 @@ from .views import (
     handle_heartbeat,
     handle_verify_push,
     handle_ic_card_info_push,
-    handle_stranger_capture
+    handle_stranger_capture,
+    handle_qr_code_push,
+    handle_alarm_push
+
 )
 
 urlpatterns = [
@@ -11,6 +14,8 @@ urlpatterns = [
     path('Subscribe/auth', handle_verify_push, name='handle_verify_push'),
     path('Subscribe/RF', handle_ic_card_info_push, name='handle_ic_card_info_push'),
     path('Subscribe/stranger', handle_stranger_capture, name='handle_stranger_capture'),
+    path('Subscribe/QRCode', handle_qr_code_push, name='handle_qrcode_push'),
+    path('Subscribe/Alarm', handle_alarm_push, name='handle_alarm_push'),
 ]
 
 # if settings.DEBUG:
