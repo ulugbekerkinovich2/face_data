7562aa32c92e9ff75fd9f196ba24f30ddaaf89e0
ulugbekerkinovich2 <ulugbek9015y@gmail.com>
2025-03-15 15:42:16 +0500

    changes schedule run download images

--- run_download_images.py
+++ run_download_images.py
@@ -1,12 +1,14 @@
 import os
 import django
+import time
 from django.core.management import call_command
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "conf.settings")  # <-- loyihangiz nomi
-
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "conf.settings")
 django.setup()
 
 if __name__ == "__main__":
-    print("⏳ Starting image sync...")
-    call_command("download_missing_images")
-    print("✅ Done.")
+    while True:
+        print("⏳ Checking missing images...")
+        call_command("download_missing_images")
+        print("✅ Done. Sleeping 10 minutes.")
+        time.sleep(180)  # 10 daqiqa
